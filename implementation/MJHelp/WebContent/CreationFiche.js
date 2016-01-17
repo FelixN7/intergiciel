488b99ccbcec24cf2103a10d3b79d26c422ce1b5
msx94 <maxime_verseau@hotmail.fr>
2016-01-17 13:47:24 +0100

    Modification de la page de création de fiche

--- implementation/MJHelp/WebContent/CreationFiche.js
+++ implementation/MJHelp/WebContent/CreationFiche.js
@@ -92,6 +92,44 @@ function setModCha() {
     }
 }
 
+function setInit() {
+	var x1, x2, text ;
+	
+	x1 = document.getElementById("dex") ;
+	x2 = document.getElementById("miscInit");
+	text = Math.floor((x1-10)/2) + x2 ;
+	document.getElementById("init").innerHTML = text ;
+}
+
+function settotaux() {
+	var x1, x1bis, x2, x2bis, x3, x3bis, x4, x4bis, x5, x5bis, x6, x6bis, text1, text2, text3, text4, text5, text6 ;
+	
+	x1 = Number(document.getElementById("dex").value) ;
+	x1bis = Number(document.getElementById("acrobatiesRank").value) ;
+	text1 = (Math.floor((x1-10)/2) + x1bis).valueOf() ;
+	document.getElementById("totalAcrobaties").innerHTML = text1 ;
+	
+	x1 = Number(document.getElementById("dex").value) ;
+	x1bis = Number(document.getElementById("acrobatiesRank").value) ;
+	text1 = (Math.floor((x1-10)/2) + x1bis).valueOf() ;
+	document.getElementById("totalAcrobaties").innerHTML = text1 ;
+	
+	x1 = Number(document.getElementById("dex").value) ;
+	x1bis = Number(document.getElementById("acrobatiesRank").value) ;
+	text1 = (Math.floor((x1-10)/2) + x1bis).valueOf() ;
+	document.getElementById("totalAcrobaties").innerHTML = text1 ;
+	
+	x1 = Number(document.getElementById("dex").value) ;
+	x1bis = Number(document.getElementById("acrobatiesRank").value) ;
+	text1 = (Math.floor((x1-10)/2) + x1bis).valueOf() ;
+	document.getElementById("totalAcrobaties").innerHTML = text1 ;
+	
+	x1 = Number(document.getElementById("dex").value) ;
+	x1bis = Number(document.getElementById("acrobatiesRank").value) ;
+	text1 = (Math.floor((x1-10)/2) + x1bis).valueOf() ;
+	document.getElementById("totalAcrobaties").innerHTML = text1 ;
+}
+
 /**
 function setHP() {
 	var x, cl, lvl, text ;
